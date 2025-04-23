0155a69a554be48cb24242acd7285feb294b3472
littlemirror-blue <3802320322@qq.com>
2025-04-23 13:09:08 +0800

    mri_train_local_srf2_epoch=100

--- SRGAN_med_mri/train.py
+++ SRGAN_med_mri/train.py
@@ -18,7 +18,7 @@ parser = argparse.ArgumentParser(description='Train Super Resolution Models')
 parser.add_argument('--crop_size', default=96, type=int, help='training images crop size')
 parser.add_argument('--upscale_factor', default=2, type=int, choices=[2, 4, 8],
                     help='super resolution upscale factor')
-parser.add_argument('--num_epochs', default=10, type=int, help='train epoch number')
+parser.add_argument('--num_epochs', default=100, type=int, help='train epoch number')
 
 if __name__ == '__main__':
     opt = parser.parse_args()
